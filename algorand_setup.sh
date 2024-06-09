8ff2d0aac051a7a98d8188d984a4df0e638766e8
jimoh-ajani <jimohajani00@gmail.com>
2024-06-09 04:20:23 -0700

    Update algorand_setup.sh

--- algorand_setup.sh
+++ algorand_setup.sh
@@ -1,21 +1,13 @@
-{
-    "forwardPorts": [4001, 4002, 8980, 5173],
-    "portsAttributes": {
-      "4001": {
-        "label": "algod"
-      },
-      "4002": {
-        "label": "kmd"
-      },
-      "8980": {
-        "label": "indexer"
-      },
-      "5173": {
-        "label": "vite"
-      }
-    },
-    
-    "postCreateCommand": "pipx install algokit",
-    "postStartCommand": "algokit localnet start"
+#!/bin/bash
 
-  }
+echo "Running setup script..."
+
+pipx install poetry
+pip install typing-extensions
+poetry init -n
+git clone https://github.com/algorandfoundation/algokit-utils-py.git
+cd algokit-utils-py
+pip install .
+cd ..
+
+echo "Setup completed."
